31143d489858dc60e732a08e6b9dd195f2d46097
Németh Tamás <tamas.nemeth2002@gmail.com>
2025-02-14 18:04:41 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import prisma from './lib/db';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 9999;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
